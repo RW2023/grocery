3515df9e387ae325d51cc48842450ecf015bbb55
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-25 13:55:41 -0500

    Update Home Page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 //src/app/page.tsx
 
 export default function Home(){
-  return(
-    <div className="container flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-5xl font-sans font-bold">Home Page</h1>
+  return (
+    <div className="container flex flex-col justify-center items-center min-h-screen bg-background">
+      <h1 className="text-5xl font-sans font-bold text-headline">Home Page</h1>
     </div>
-  )
+  );
 }
