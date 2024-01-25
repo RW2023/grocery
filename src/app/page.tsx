e1926a58e6d3ed165423da264675fb46184a47a6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-25 14:35:30 -0500

    Refactor home page layout and add grocery app title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,21 @@
 
 export default function Home(){
   return (
-    <div className="container flex flex-col justify-center items-center min-h-screen bg-background">
-      <h1 className="text-5xl font-sans font-bold text-headline">Home Page</h1>
+    <>
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-5xl m-1 p-1">Grocery App</h1>
     </div>
+    <div className="container min-h-screen bg-background rounded border border-1 mx-auto grid md:grid-cols-3 grid-cols-1 gap-4">
+      <div className="col-span-3 md:col-span-1">
+        <div className="border border-1">box 1</div>
+      </div>
+      <div className="col-span-3 md:col-span-1">
+        <div className="border border-1">box 2</div>
+      </div>
+      <div className="col-span-3 md:col-span-1">
+        <div className="border border-1">box 3</div>
+      </div>
+    </div>
+    </>
   );
 }
