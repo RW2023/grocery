920c0cfdd474759086f78427b277eff8762362dc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-25 15:02:43 -0500

    Refactor grid layout and add padding to boxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ export default function Home(){
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-5xl m-1 p-1">Grocery App</h1>
     </div>
-    <div className="container min-h-screen bg-background rounded border border-1 mx-auto grid md:grid-cols-3 grid-cols-1 gap-4">
-      <div className="col-span-3 md:col-span-1">
-        <div className="border border-1">box 1</div>
+    <div className="container min-h-screen bg-background rounded border border-1 mx-auto grid md:grid-cols-3 grid-cols-1 gap-4 m-5">
+      <div className="col-span-3 md:col-span-1 p-4">
+        <div className="border border-1 p-1">box 1</div>
       </div>
-      <div className="col-span-3 md:col-span-1">
-        <div className="border border-1">box 2</div>
+      <div className="col-span-3 md:col-span-1 p-4">
+        <div className="border border-1 p-1">box 2</div>
       </div>
-      <div className="col-span-3 md:col-span-1">
-        <div className="border border-1">box 3</div>
+      <div className="col-span-3 md:col-span-1 p-4">
+        <div className="border border-1 p-1">box 3</div>
       </div>
     </div>
     </>
