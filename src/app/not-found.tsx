867c2b4c84e2a8147777ce66664c1ee22df7d7f1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-25 23:00:58 -0500

    add border to not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,8 +8,8 @@ function Custom404() {
       className="bg-background min-h-screen flex items-center justify-center text-center "
       data-theme="halloween"
     >
-      <div className="card bordered p-10 shadow-xl bg-base-300 mx-auto w-3/4">
-        <div className="border rounded p-4 bg-background">
+      <div className="card bordered border-border p-10 shadow-xl bg-base-300 mx-auto w-3/4">
+        <div className="border-border rounded p-4 bg-background">
           <SubHeading title="Oh oh!🥹" iconClass="fas fa-exclamation-triangle" />
         </div>
         <SubHeading title="The page you're looking for cannot be found" />
