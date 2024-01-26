d72d6f0f6c903d8a2b5e9d563a218ab561fd4620
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-26 00:31:25 -0500

    Add UpdateGroceryItem component

--- src/app/devdash/page.tsx
+++ src/app/devdash/page.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import Heading from '@/Components/Ui/Heading';
 import AddGroceryItem from '@/Components/Forms/AddGroceryItem';
 import GroceryListDisplay from '@/Components/Ui/GroceryListDisplay';
+import UpdateGroceryItem from '@/Components/Forms/UpdateGroceryItem';
 
 interface Props {}
 
@@ -9,8 +10,9 @@ const page: FC<Props> = (): JSX.Element => {
     return (
         <div className='min-h-screen bg-background'>
             <Heading title='DevDash' />
-            <AddGroceryItem />
             <GroceryListDisplay />
+            <AddGroceryItem />
+            <UpdateGroceryItem />
         </div>
     );
 };
