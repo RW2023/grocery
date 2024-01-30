ba3cf95411f941f32848054d5b2a283b6623f281
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-30 16:15:15 -0500

    Refactor login page component to match docs on supabase

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,80 +1,116 @@
 'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import {
-  createClientComponentClient,
-  SupabaseClient,
-} from '@supabase/auth-helpers-nextjs';
 
-const Auth: React.FC = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>('');
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [feedback, setFeedback] = useState('');
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
-  const supabase: SupabaseClient = createClientComponentClient();
+  const handleSignUp = async () => {
+    setFeedback('');
+    const response = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
 
-  const handleLogin = async () => {
-    setLoading(true);
-    setError('');
+    if (response.error) {
+      setFeedback('Signup failed: ' + response.error.message);
+    } else if (response.data.user) {
+      setFeedback('Signup successful. Redirecting to dashboard...');
+      router.push('/devdash');
+    }
+  };
+
+  const handleSignIn = async () => {
+    setFeedback('');
     const response = await supabase.auth.signInWithPassword({
       email,
       password,
     });
+
     if (response.error) {
-      setError(response.error.message);
+      setFeedback('Login failed: ' + response.error.message);
+    } else if (response.data.user) {
+      setFeedback('Login successful. Redirecting to dashboard...');
+      router.push('/devdash');
     }
-    setLoading(false);
   };
 
-  const handleSignup = async () => {
-    setLoading(true);
-    setError('');
-    const response = await supabase.auth.signUp({ email, password });
-    if (response.error) {
-      setError(response.error.message);
-    }
-    setLoading(false);
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
   };
 
   return (
-    <div className="bg-background p-10 rounded-md w-fit mx-auto justify-center items-center mt-6 border border-1 drop-shadow-md">
-      {error && <p className="text-red-500 mb-4">{error}</p>}
-      <div className="mb-4">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="input input-bordered w-full max-w-xs"
-          disabled={loading}
-        />
-      </div>
-      <div className="mb-6">
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="input input-bordered w-full max-w-xs"
-          disabled={loading}
-        />
-      </div>
-      <button
-        onClick={handleLogin}
-        className="btn btn-primary mr-2"
-        disabled={loading}
-      >
-        {loading ? 'Logging in...' : 'Login'}
-      </button>
-      <button
-        onClick={handleSignup}
-        className="btn btn-secondary"
-        disabled={loading}
-      >
-        {loading ? 'Signing up...' : 'Sign Up'}
-      </button>
+    <div className="bg-background p-10 rounded-md w-full max-w-xs mx-auto mt-6 border border-1 drop-shadow-md">
+      <form className="space-y-4">
+        {feedback && <p className="text-red-500 text-center">{feedback}</p>}
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="input input-bordered w-full"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className="input input-bordered w-full"
+            required
+          />
+        </div>
+        <div className="flex flex-col space-y-2">
+          <button
+            type="button"
+            onClick={handleSignUp}
+            className="btn btn-primary"
+          >
+            Sign up
+          </button>
+          <button
+            type="button"
+            onClick={handleSignIn}
+            className="btn btn-secondary"
+          >
+            Sign in
+          </button>
+          <button
+            type="button"
+            onClick={handleSignOut}
+            className="btn btn-accent"
+          >
+            Sign out
+          </button>
+        </div>
+      </form>
     </div>
   );
-};
-
-export default Auth;
+}
