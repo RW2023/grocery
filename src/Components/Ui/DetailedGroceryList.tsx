4f0473114576037f60c846f114262e15894f44c0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-02-01 15:42:20 -0500

    Refactor DetailedGroceryListDisplay component to include health filter

--- src/Components/Ui/DetailedGroceryList.tsx
+++ src/Components/Ui/DetailedGroceryList.tsx
@@ -1,4 +1,4 @@
-// src/Components/Ui/DetailedGroceryListDisplay.tsx
+//src/Components/Ui/DetailedGroceryList.tsx
 'use client';
 import React, { useState, useEffect } from 'react';
 import { supabase } from '@/utils/supabaseClient';
@@ -11,6 +11,9 @@ const DetailedGroceryListDisplay = () => {
 
   const [groceryItems, setGroceryItems] = useState<GroceryItem[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [healthFilter, setHealthFilter] = useState<
+    'all' | 'healthy' | 'unhealthy'
+  >('all');
   const [loading, setLoading] = useState(true);
 
   const fetchGroceryItems = async () => {
@@ -33,34 +36,49 @@ const DetailedGroceryListDisplay = () => {
     setSearchQuery(e.target.value.toLowerCase());
   };
 
-  const filteredGroceryItems = searchQuery
-    ? groceryItems.filter((item) =>
-        item.name.toLowerCase().includes(searchQuery),
-      )
-    : groceryItems;
+  const handleHealthFilterChange = (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setHealthFilter(e.target.value as 'all' | 'healthy' | 'unhealthy');
+  };
+
+const filteredGroceryItems = groceryItems.filter(
+  (item) =>
+    item.name.toLowerCase().includes(searchQuery) &&
+    (healthFilter === 'all' ||
+      (healthFilter === 'healthy' && item.Health === true) ||
+      (healthFilter === 'unhealthy' && item.Health === false)),
+);
 
   if (loading) return <Loading />;
 
   return (
     <div className="border-border border-2 rounded p-3 bg-base-300 m-3">
       <SubHeading title="Detailed Inventory" />
-      <div className="flex flex-col justify-center items-center mb-4">
+      <div className="flex flex-col md:flex-row justify-between items-center mb-4">
         <input
           type="text"
           placeholder="Search items..."
-          className="input input-bordered border-border w-full sm:w-2/3 md:w-2/3 lg:w-2/3"
+          className="input input-bordered border-border w-full mb-2 md:mb-0 md:w-1/3"
           value={searchQuery}
           onChange={handleSearchChange}
         />
+        <select
+            title="Health Filter"
+          className="select select-bordered w-full md:w-1/3"
+          value={healthFilter}
+          onChange={handleHealthFilterChange}
+        >
+          <option value="all">All Items</option>
+          <option value="healthy">Healthy</option>
+          <option value="unhealthy">Unhealthy</option>
+        </select>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {filteredGroceryItems.map((item) => (
           <div key={item.id} className="card bg-base-100 shadow-xl">
             <div className="card-body">
-              <h2 className="card-title">
-                {item.name}
-                <div className="badge badge-secondary">ID: {item.id}</div>
-              </h2>
+              <h2 className="card-title">{item.name}</h2>
               <p>Quantity: {item.quantity}</p>
               <p>Food Group: {item.Food_Group || 'N/A'}</p>
               <p>Health: {item.Health ? 'Healthy' : 'Unhealthy'}</p>
