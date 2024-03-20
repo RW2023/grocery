594cff3148c914b0f71285ca1d535f5435772a94
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-20 08:23:35 -0400

    Add LoadingInline component and handle isLoading prop in MealsList

--- src/Components/Ui/MealsList.tsx
+++ src/Components/Ui/MealsList.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import SubHeading from '@/Components/Ui/SubHeading'; // Adjust the import path if necessary
+import React, { useState } from 'react';
+import SubHeading from '@/Components/Ui/SubHeading'; // Make sure this path is correct
+import LoadingInline from '@/Components/Ui/LoadingInline'; // Make sure this path is correct
 
 interface Meal {
   id: number;
@@ -10,22 +11,27 @@ interface Meal {
 
 interface MealsListProps {
   meals: Meal[];
+  isLoading: boolean; // Pass isLoading as a prop if it's managed by a parent component
 }
 
-const MealsList: React.FC<MealsListProps> = ({ meals }) => {
+const MealsList: React.FC<MealsListProps> = ({ meals, isLoading }) => {
   return (
-    <div className="text-base-content my-8">
+    <div className="text-base-content my-8 p-1 bg-base-300 rounded border">
       <SubHeading title="Meals for the Week" />
       <div className="flex flex-col items-center justify-center">
-        <ul className="list-none">
-          {meals.map((meal) => (
-            <li key={meal.id} className="mb-2 bg-base-300">
-              <span className="font-semibold">{meal.date}</span> -{' '}
-              {meal.meal_type} - Recipe ID:{' '}
-              <span className="badge badge-outline">{meal.recipe_id}</span>
-            </li>
-          ))}
-        </ul>
+        {isLoading ? (
+          <LoadingInline />
+        ) : (
+          <ul className="list-none">
+            {meals.map((meal) => (
+              <li key={meal.id} className="mb-2 bg-base-300">
+                <span className="font-semibold">{meal.date}</span> -{' '}
+                {meal.meal_type} - Recipe ID:{' '}
+                <span className="badge badge-outline">{meal.recipe_id}</span>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
