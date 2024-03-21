7757b8d4df91d8469ad73d53aa39ee961f072275
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-20 21:38:36 -0400

    Remove commented out code in Footer component

--- src/Components/Ui/Footer.tsx
+++ src/Components/Ui/Footer.tsx
@@ -3,28 +3,6 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    // <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
-    //   <nav className="grid grid-flow-col gap-4">
-    //     {/* <Link href="/about">About</Link>
-    //     <Link href="/contact">Contact</Link>
-    //     <Link href="/feedback">Feedback</Link> */}
-    //   </nav>
-    //   <nav>
-    //     <div className="grid grid-flow-col gap-4 text-4xl">
-    //       {/* GitHub */}
-    //       <a
-    //         href="https://github.com/RW2023/kids"
-    //         target="https://github.com/RW2023/kids"
-    //         rel="noopener noreferrer"
-    //         aria-label="GitHub"
-    //       >
-    //         <i className="fab fa-github"></i>
-    //         <span className="sr-only">GitHub Repo Button</span>
-    //       </a>
-    //     </div>
-    //   </nav>
-
-    // </footer>
     <footer className="footer footer-center p-4 bg-base-300 text-base-content h-full">
       <aside>
         <p>
