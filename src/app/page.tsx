d800ef39029cfd7657c655d9efb62d6632194369
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-20 21:37:22 -0400

    Refactor page.tsx to use proper list styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col justify-center items-center m-3">
-       <Heading title ='grocery & wine' />
+        <Heading title="grocery & wine" />
       </div>
       <div className="container bg-background rounded-lg border border-border mx-auto grid md:grid-cols-3 grid-cols-1 gap-4 p-5">
         {/* Section 1 */}
         <SectionCard
           headingTitle="Shopping List"
           content={
-            <ul>
+            <ul className="list-disc list-inside">
               <li>Organize your grocery list</li>
               <li>Sort by aisle or category</li>
               <li>Check off items as you shop</li>
@@ -28,7 +28,7 @@ export default function Home() {
         <SectionCard
           headingTitle="Grocery Inventory"
           content={
-            <ul>
+            <ul className="list-disc list-inside">
               <li>Keep track of what you have</li>
               <li>Analyze and visualize</li>
               <li>Discover over and under stock</li>
@@ -42,7 +42,7 @@ export default function Home() {
         <SectionCard
           headingTitle="Meal Planner"
           content={
-            <ul>
+            <ul className="list-disc list-inside">
               <li>Organize Weekly Meals</li>
               <li>Calendar view to see 30 days in advance</li>
               <li>Collect family meal data for analysis</li>
@@ -53,10 +53,13 @@ export default function Home() {
         />
       </div>
       <div className="container flex flex-col justify-center items-center p-4 my-6 mx-auto bg-background rounded-lg border border-border list-none">
-        <div className="card card-compact bg-base-100 shadow-xl w-full p-4">
+        <div className="card card-compact bg-base-100 shadow-xl w-full p-4 list-disc list-inside">
           <SubHeading title="Inventory Dashboard" />
-          <li>Crud operations for inventory</li>
-          <li>Search inventory items by name or category</li>
+          <ul className=" list-disc list-inside">
+            <li>Crud operations for inventory</li>
+            <li>Search inventory items by name or category</li>
+            <li>More features will be addded</li>
+          </ul>
           <div className="card-actions justify-end">
             <button
               type="button"
